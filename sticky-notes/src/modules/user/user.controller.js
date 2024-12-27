7afc4c6645b2b9fc38b6aa3c1ca318076f65febf
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2024-12-27 17:37:46 +0200

    // fix bug: check if user is exist before comparing the password to avoid error user?.password is undefined
    Refactor user authentication methods in user.controller.js
    
    - Updated the signup function to return the response directly, improving clarity.
    - Enhanced the signin function to check for user existence before password comparison, preventing potential errors.
    - Ensured consistent response structure for successful authentication while maintaining security best practices.

--- sticky-notes/src/modules/user/user.controller.js
+++ sticky-notes/src/modules/user/user.controller.js
@@ -5,15 +5,13 @@ const signup = async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
   const user = await User.insertMany({ ...req.body, password: hashedPassword });
   user[0].password = undefined; // to hide the password from the response
-  res.status(201).json({ message: "success", user: user[0] }); //insertMany returns an array of the inserted documents, so we need to access the first element of the array.
+  return res.status(201).json({ message: "success", user: user[0] }); //insertMany returns an array of the inserted documents, so we need to access the first element of the array.
 };
 
 const signin = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-  const isPasswordCorrect = await bcrypt.compare(
-    req.body.password,
-    user.password
-  );
+  const user = await User.findOne({ email: req.body.email }); // object or null
+  const isPasswordCorrect =
+    user && (await bcrypt.compare(req.body.password, user.password)); // fix bug: check if user is exist before comparing the password to avoid error user?.password is undefined
   if (user && isPasswordCorrect) {
     return res.status(200).json({ message: "success", user });
   }
