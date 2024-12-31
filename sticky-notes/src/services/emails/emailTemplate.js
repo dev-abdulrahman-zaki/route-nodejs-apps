7866540e4a40c90d09f90deb775f5f40b290de23
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2024-12-31 14:17:05 +0200

    Enhance email verification template with additional information
    - Updated emailTemplate.js to include a message for users who did not request the email.
    - Added a note indicating that the verification link will expire in 1 hour.
    - Included a statement clarifying that the email was sent due to account signup, improving user awareness and security.

--- sticky-notes/src/services/emails/emailTemplate.js
+++ sticky-notes/src/services/emails/emailTemplate.js
@@ -2,5 +2,10 @@ export const emailTemplate = (token) => {
   return `
   <h1>Hello</h1>
   <p>Click <a href="http://localhost:4000/auth/verify/${token}">here</a> to verify your email</p>
+  <div>
+    <p>If you did not request this, please ignore this email.</p>
+    <p>This email was sent to ${email} because you signed up for a Sticky Notes account.</p>
+    <span>The link will expire in 1 hour.</span>
+  </div>
   `;
 };
