cd24fdc604190b556c30cc113363195bc88c1ef3
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2024-12-31 15:33:54 +0200

    Enhance global error handling in sticky notes application
    - Updated globalError middleware to include stack trace in development mode for improved debugging.
    - Added NODE_ENV variable to .env file to manage environment configurations effectively.

--- sticky-notes/src/middlewares/globalError.js
+++ sticky-notes/src/middlewares/globalError.js
@@ -1,7 +1,10 @@
 export const globalError = (err, req, res, next) => {
   //   console.error(err);
   console.error(err.stack);
-  res
-    .status(err.statusCode || 500)
-    .json({ message: err.message, statusCode: err.statusCode });
+  res.status(err.statusCode || 500).json({
+    message: err.message,
+    statusCode: err.statusCode,
+    // Display stack in development mode
+    stack: process.env.NODE_ENV === "development" ? err.stack : null,
+  });
 };
