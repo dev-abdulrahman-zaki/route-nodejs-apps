1e8d2523889fd4b59bab3dd3d73f4fd0ea1175b8
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-01 01:59:07 +0200

    Refactor index.js to improve structure and clarity of middleware and error handling
    - Added comments to delineate sections for uncaught exceptions, middleware, route definitions, and error handling.
    - Organized middleware and route definitions for better readability and maintainability.
    - Ensured consistent error handling practices are followed throughout the application.

--- sticky-notes/index.js
+++ sticky-notes/index.js
@@ -1,8 +1,10 @@
+// 00. Uncaught Exception
 process.on("uncaughtException", (err) => {
   console.error("Uncaught Exception:", err);
   console.error(err?.stack);
 });
 
+// 01. Importing the required modules
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -12,30 +14,32 @@ import noteRoutes from "./src/modules/note/note.routes.js";
 import { checkAuth } from "./src/middlewares/checkAuth.js";
 import { SystemError } from "./src/utils/systemError.js";
 import { globalError } from "./src/middlewares/globalError.js";
+
+// 02. Creating the express app
 const app = express();
 const port = 4000;
 
-// 00. Middleware
+// 03. Middleware
 app.use(express.json());
 
-// 01. Define the base path
+// 04. Define the base path
 app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/auth", userRoutes);
 app.use("/notes", checkAuth, noteRoutes);
 
-// 02. Catch all routes
+// 05. Catch all routes
 app.use("*", (req, res, next) => {
   next(new SystemError(`Route not found: ${req.originalUrl}`, 404));
 });
 
-// 03. Error handling middleware
+// 06. Error handling middleware
 app.use(globalError);
 
-// 04. Unhandled Rejection
+// 07. Unhandled Rejection
 process.on("unhandledRejection", (err) => {
   console.error("Unhandled Rejection:", err);
   console.error(err?.stack);
 });
 
-// Start the server
+// 08. Start the server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
