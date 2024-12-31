43f443ab7767eec5e44e169c60fc7028fe2755fe
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2024-12-31 10:33:51 +0200

    Refactor signin logic in user.controller.js to enhance security and response handling
    - Updated signin function to ensure user existence is checked before password comparison, preventing potential errors.
    - Removed password from the response to enhance security by not exposing sensitive information.
    - Maintained consistent response structure for successful authentication while adhering to security best practices.

--- sticky-notes/src/modules/user/user.controller.js
+++ sticky-notes/src/modules/user/user.controller.js
@@ -9,12 +9,13 @@ const signup = async (req, res) => {
 };
 
 const signin = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email }); // object or null
+  const user = await User.findOne({ email: req.body.email },); // object or null
   const isPasswordCorrect =
     user && (await bcrypt.compare(req.body.password, user.password)); // fix bug: check if user is exist before comparing the password to avoid error user?.password is undefined
   if (!user || !isPasswordCorrect) {
     return res.status(401).json({ message: "Invalid email or password" }); // security best practice: do not reveal the reason for the failure
   }
+  user.password = undefined; // to hide the password from the response
   return res.status(200).json({ message: "success", user });
 };
 
