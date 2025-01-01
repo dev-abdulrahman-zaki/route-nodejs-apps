b1354df48c0ba0764a159d67240bf5cb0f1305ea
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-01 16:03:34 +0200

    Enhance validation middleware to improve error handling and reporting
    - Updated the validation middleware to include the abortEarly option for comprehensive error reporting.
    - Refactored error handling to return an array of error messages using SystemError for better clarity in responses.
    - Improved overall structure and maintainability of the validation logic.

--- sticky-notes/src/middlewares/validate.js
+++ sticky-notes/src/middlewares/validate.js
@@ -1,8 +1,14 @@
+import { SystemError } from "../utils/systemError.js";
+
 export const validate = (schema) => {
   return (req, res, next) => {
-    const { error } = schema.validate(req.body);
+    const { error } = schema.validate(req.body, {
+      abortEarly: false,
+    });
     if (error) {
-      return res.status(400).json({ message: error?.details });
+      const errorMessages = error?.details.map((err) => err.message);
+      //   res.status(400).json({ message: errorMessages });
+      next(new SystemError(errorMessages, 400));
     }
     next();
   };
