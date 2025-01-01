18db666556608df56d5e69bbb4cf63cd9c541be3
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-01 09:52:33 +0200

    Update environment configuration and database connection handling
    - Added MONGODB_URI_CONNECTION_STRING to .env file for improved database connection management.
    - Refactored dbConnection.js to use the new environment variable for MongoDB connection, enhancing flexibility and security.
    - Improved error logging in dbConnection.js to provide clearer error messages during database connection failures.

--- sticky-notes/database/dbConnection.js
+++ sticky-notes/database/dbConnection.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose"; // or: import {connect} from "mongoose";
 
 // 01. Connect to MongoDB
 export const dbConnection = mongoose
-  .connect("mongodb://127.0.0.1:27017/sticky-notes") // or: mongodb://localhost:27017/sticky-notes
+  .connect(process.env.MONGODB_URI_CONNECTION_STRING) // or: mongodb://localhost:27017/sticky-notes
   .then(() => {
     console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.log(err);
+    console.error('Database connection error:', err);
   });
