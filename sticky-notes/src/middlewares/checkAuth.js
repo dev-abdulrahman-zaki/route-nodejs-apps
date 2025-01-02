40a664c4ea5bcc09872608dda659a532bebea141
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-02 16:02:04 +0200

    Refactor checkAuth middleware to improve error handling and clarity
    - Updated the checkAuth middleware to throw a SystemError instead of returning a JSON response when no token is provided, enhancing error management.
    - Improved code readability by clarifying the flow of the middleware and ensuring consistent use of the next() function for better middleware chaining.

--- sticky-notes/src/middlewares/checkAuth.js
+++ sticky-notes/src/middlewares/checkAuth.js
@@ -5,12 +5,13 @@ export const checkAuth = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
   const token = authHeader?.split(" ")[1] || null;
   if (!token) {
-    return res.status(401).json({ message: "No token provided" });
+    // return res.status(401).json({ message: "No token provided" });
+    return next(new SystemError("No token provided", 401));
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) return res.status(401).json({ message: "Invalid token"});
     // add the decoded user to the request object (modify the request object in middleware) - modifies/transforms the request object
     req.user = decoded;
-    next();
+    return next();
   });
 };
