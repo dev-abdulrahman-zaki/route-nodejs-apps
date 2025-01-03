5765501ca54263d9d4e09ff412ff15dc96e69a4a
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-03 06:42:37 +0200

    Refactor photo upload routes to use disk storage with unique filenames
    - Updated multer configuration to utilize disk storage, allowing for unique filenames based on timestamp and random number.
    - Removed the previous upload configuration for improved file management.
    - Enhanced the clarity of the photo upload process by ensuring files are stored with unique identifiers.

--- sticky-notes/src/modules/photo/photo.routes.js
+++ sticky-notes/src/modules/photo/photo.routes.js
@@ -3,9 +3,19 @@ import multer from "multer";
 import { uploadPhoto, uploadPhotos, getPhotos } from "./photo.controller.js";
 
 const photoRoutes = express.Router();
-const upload = multer({
-  dest: "uploads/",
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
 });
+
+const upload = multer({ storage: storage });
+
 photoRoutes.post(`/upload-single`, upload.single("photo"), uploadPhoto); // Note: photo is the name of the file input in the form. (not imgUrl in the schema)
 photoRoutes.post(`/upload-multiple`, upload.array("photos"), uploadPhotos);
 photoRoutes.get(`/`, getPhotos);
