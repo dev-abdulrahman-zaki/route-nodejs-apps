e7c397ddbde7b0df0695a0f3cc5a8afca18beac5
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-08 15:06:35 +0200

    Enhance category controller error handling in E-Commerce application
    - Updated getAllCategories, getSingleCategory, and updateCategory methods to include error handling for cases where categories are not found, returning a 404 status with a descriptive SystemError.
    - Improved code consistency by adding 'next' parameter to error-handling functions.

--- E-Commerce/src/modules/category/category.controller.js
+++ E-Commerce/src/modules/category/category.controller.js
@@ -14,27 +14,36 @@ const addCategory = catchError(async (req, res) => {
   res.status(201).json({ message: "success", category });
 });
 
-const getAllCategories = catchError(async (req, res) => {
+const getAllCategories = catchError(async (req, res, next) => {
   const categories = await Category.find().populate(
     "createdBy",
     "-password -email -confirmEmail -createdAt"
   ); // or : const notes = await Note.find().populate("user", { password: 0 });
+  if (!categories) {
+    return next(new SystemError("Categories not found", 404));
+  }
   res.status(200).json({ message: "success", categories });
 });
 
-const getSingleCategory = catchError(async (req, res) => {
+const getSingleCategory = catchError(async (req, res, next) => {
   const category = await Category.findOne({ slug: req.params.slug }).populate(
     "createdBy",
     "-password -email -confirmEmail -createdAt"
   );
+  if (!category) {
+    return next(new SystemError("Category not found", 404));
+  }
   res.status(200).json({ message: "success", category });
 });
 
-const updateCategory = catchError(async (req, res) => {
+const updateCategory = catchError(async (req, res, next) => {
   req.body.slug = slugify(req.body.name, { lower: true });
   const category = await Category.findOneAndUpdate({ slug: req.params.slug }, req.body, {
     new: true,
   });
+  if (!category) {
+    return next(new SystemError("Category not found", 404));
+  }
   res.status(200).json({ message: "success", category });
 });
 
