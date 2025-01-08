3a5c99d6c085dcb64ce50ad46becc9955c3004c5
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-08 22:15:18 +0200

    Enhance brand validation and middleware in E-Commerce application
    - Updated brand validation schemas to include detailed logo object validation for both creation and updates.
    - Integrated validation middleware in brand routes to ensure proper handling of logo uploads during brand operations.
    - Refactored existing code to improve clarity and maintainability of brand management functionality.

--- E-Commerce/src/modules/brand/brand.routes.js
+++ E-Commerce/src/modules/brand/brand.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import fileUpload from "../../services/fileUpload/fileUpload.js";
-// import { validate } from "../../middlewares/validate.js";
-// import { addCategoryValidationSchema, updateCategoryValidationSchema } from "./brand.validation.js";
+import { validate } from "../../middlewares/validate.js";
+import { addBrandValidationSchema, updateBrandValidationSchema } from "./brand.validation.js";
 const brandRoutes = express.Router();
 
 import {
@@ -12,10 +12,10 @@ import {
   deleteBrand,
 } from "./brand.controller.js";
 
-brandRoutes.post(`/`, fileUpload("brands").single("logo"), addBrand);
+brandRoutes.post(`/`, fileUpload("brands").single("logo"), validate(addBrandValidationSchema, "logo"), addBrand);
 brandRoutes.get(`/`, getAllBrands);
 brandRoutes.get(`/:slug`, getSingleBrand);
-brandRoutes.put(`/:slug`, fileUpload("brands").single("logo"), updateBrand);
+brandRoutes.put(`/:slug`, fileUpload("brands").single("logo"), validate(updateBrandValidationSchema, "logo"), updateBrand);
 brandRoutes.delete(`/:slug`, deleteBrand);
 
 export default brandRoutes;
--- E-Commerce/src/modules/brand/brand.validation.js
+++ E-Commerce/src/modules/brand/brand.validation.js
@@ -1,15 +1,35 @@
 import Joi from "joi";
 
-const addCategoryValidationSchema = Joi.object({
+const addBrandValidationSchema = Joi.object({
   name: Joi.string().required().trim().min(3),
-  slug: Joi.string().required().trim().min(3),
-  image: Joi.string().required().trim(),
+  logo: Joi.object({
+    fieldname: Joi.string().required(),
+    originalname: Joi.string().required(),
+    destination: Joi.string().required(),
+    filename: Joi.string().required(),
+    mimetype: Joi.string()
+      .valid("image/jpeg", "image/png", "image/jpg")
+      .required(),
+    encoding: Joi.string().required(),
+    size: Joi.number().max(1000000).required(),
+    path: Joi.string().required(),
+  }).required(),
 });
 
-const updateCategoryValidationSchema = Joi.object({
+const updateBrandValidationSchema = Joi.object({
   name: Joi.string().trim().min(3),
-  slug: Joi.string().trim().min(3),
-  image: Joi.string().trim(),
+  logo: Joi.object({
+    fieldname: Joi.string().required(),
+    originalname: Joi.string().required(),
+    destination: Joi.string().required(),
+    filename: Joi.string().required(),
+    mimetype: Joi.string()
+      .valid("image/jpeg", "image/png", "image/jpg")
+      .required(),
+    encoding: Joi.string().required(),
+    size: Joi.number().max(1000000).required(),
+    path: Joi.string().required(),
+  }),
 });
 
-export { addCategoryValidationSchema, updateCategoryValidationSchema };
+export { addBrandValidationSchema, updateBrandValidationSchema };
