ea3f2ce9c02ec8be1a96737b009c050824d928aa
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-19 19:35:56 +0200

    Refactor product validation schemas to include nested structure for params, query, and body. Ensure required fields and validation rules are consistently applied across add and update schemas, enhancing overall data integrity.

--- E-Commerce/src/modules/product/product.validation.js
+++ E-Commerce/src/modules/product/product.validation.js
@@ -2,86 +2,94 @@ import Joi from "joi";
 import handleValidValue from "../../utils/handleValidValue.js";
 
 const addProductValidationSchema = Joi.object({
-  name: Joi.string().required().trim().min(3),
-  description: Joi.string().required().trim().min(10).max(2000),
-  imageCover: Joi.object({
-    fieldname: Joi.string().required(),
-    originalname: Joi.string().required(),
-    destination: Joi.string().required(),
-    filename: Joi.string().required(),
-    mimetype: Joi.string()
-      .valid("image/jpeg", "image/png", "image/jpg")
+  params: Joi.object().default({}),
+  query: Joi.object().default({}),
+  body: Joi.object({
+    name: Joi.string().required().trim().min(3),
+    description: Joi.string().required().trim().min(10).max(2000),
+    imageCover: Joi.object({
+      fieldname: Joi.string().required(),
+      originalname: Joi.string().required(),
+      destination: Joi.string().required(),
+      filename: Joi.string().required(),
+      mimetype: Joi.string()
+        .valid("image/jpeg", "image/png", "image/jpg")
+        .required(),
+      encoding: Joi.string().required(),
+      size: Joi.number().max(1000000).required(),
+      path: Joi.string().required(),
+    }).required(),
+    images: Joi.array()
+      .items(
+        Joi.object({
+          fieldname: Joi.string().required(),
+          originalname: Joi.string().required(),
+          destination: Joi.string().required(),
+          filename: Joi.string().required(),
+          mimetype: Joi.string()
+            .valid("image/jpeg", "image/png", "image/jpg")
+            .required(),
+          encoding: Joi.string().required(),
+          size: Joi.number().max(1000000).required(),
+          path: Joi.string().required(),
+        })
+      )
+      .min(1)
       .required(),
-    encoding: Joi.string().required(),
-    size: Joi.number().max(1000000).required(),
-    path: Joi.string().required(),
-  }).required(),
-  images: Joi.array()
-    .items(
-      Joi.object({
-        fieldname: Joi.string().required(),
-        originalname: Joi.string().required(),
-        destination: Joi.string().required(),
-        filename: Joi.string().required(),
-        mimetype: Joi.string()
-          .valid("image/jpeg", "image/png", "image/jpg")
-          .required(),
-        encoding: Joi.string().required(),
-        size: Joi.number().max(1000000).required(),
-        path: Joi.string().required(),
-      })
-    )
-    .min(1)
-    .required(),
-  price: Joi.number().required().min(0),
-  priceAfterDiscount: Joi.number().required().min(0),
-  stock: Joi.number().required().min(0),
-  category: Joi.string().hex().length(24).required(),
-  brand: Joi.string().hex().length(24).required(),
-  subcategory: Joi.string().hex().length(24).required(),
-  ratingsAverage: Joi.number().min(0).max(5),
-  ratingsQuantity: Joi.number().min(0),
+    price: Joi.number().required().min(0),
+    priceAfterDiscount: Joi.number().required().min(0),
+    stock: Joi.number().required().min(0),
+    category: Joi.string().hex().length(24).required(),
+    brand: Joi.string().hex().length(24).required(),
+    subcategory: Joi.string().hex().length(24).required(),
+    ratingsAverage: Joi.number().min(0).max(5),
+    ratingsQuantity: Joi.number().min(0),
+  }).default({}),
 });
 
 const updateProductValidationSchema = Joi.object({
-  name: Joi.string().trim().min(3),
-  description: Joi.string().trim().min(10).max(2000),
-  imageCover: Joi.object({
-    fieldname: Joi.string().required(),
-    originalname: Joi.string().required(),
-    destination: Joi.string().required(),
-    filename: Joi.string().required(),
-    mimetype: Joi.string()
-      .valid("image/jpeg", "image/png", "image/jpg")
-      .required(),
-    encoding: Joi.string().required(),
-    size: Joi.number().max(1000000).required(),
-    path: Joi.string().required(),
-  }),
-  images: Joi.array()
-    .items(
-      Joi.object({
-        fieldname: Joi.string().required(),
-        originalname: Joi.string().required(),
-        destination: Joi.string().required(),
-        filename: Joi.string().required(),
-        mimetype: Joi.string()
-          .valid("image/jpeg", "image/png", "image/jpg")
-          .required(),
-        encoding: Joi.string().required(),
-        size: Joi.number().max(1000000).required(),
-        path: Joi.string().required(),
-      })
-    )
-    .min(1),
-  price: Joi.number().min(0),
-  priceAfterDiscount: Joi.number().min(0),
-  stock: Joi.number().min(0),
-  category: Joi.string().hex().length(24),
-  brand: Joi.string().hex().length(24),
-  subcategory: Joi.string().hex().length(24),
-  ratingsAverage: Joi.number().min(0).max(5),
-  ratingsQuantity: Joi.number().min(0),
+  params: Joi.object().default({}),
+  query: Joi.object().default({}),
+  body: Joi.object({
+    name: Joi.string().trim().min(3),
+    description: Joi.string().trim().min(10).max(2000),
+    imageCover: Joi.object({
+      fieldname: Joi.string().required(),
+      originalname: Joi.string().required(),
+      destination: Joi.string().required(),
+      filename: Joi.string().required(),
+      mimetype: Joi.string()
+        .valid("image/jpeg", "image/png", "image/jpg")
+        .required(),
+      encoding: Joi.string().required(),
+      size: Joi.number().max(1000000).required(),
+      path: Joi.string().required(),
+    }),
+    images: Joi.array()
+      .items(
+        Joi.object({
+          fieldname: Joi.string().required(),
+          originalname: Joi.string().required(),
+          destination: Joi.string().required(),
+          filename: Joi.string().required(),
+          mimetype: Joi.string()
+            .valid("image/jpeg", "image/png", "image/jpg")
+            .required(),
+          encoding: Joi.string().required(),
+          size: Joi.number().max(1000000).required(),
+          path: Joi.string().required(),
+        })
+      )
+      .min(1),
+    price: Joi.number().min(0),
+    priceAfterDiscount: Joi.number().min(0),
+    stock: Joi.number().min(0),
+    category: Joi.string().hex().length(24),
+    brand: Joi.string().hex().length(24),
+    subcategory: Joi.string().hex().length(24),
+    ratingsAverage: Joi.number().min(0).max(5),
+    ratingsQuantity: Joi.number().min(0),
+  }).default({}),
 });
 
 const validSortFields = [
