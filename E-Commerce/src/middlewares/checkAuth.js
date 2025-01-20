ae0c4dd53d59dbc20b08e2cdccd3ca25fff6a694
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-20 16:00:15 +0200

    refactor checkAuth

--- E-Commerce/src/middlewares/checkAuth.js
+++ E-Commerce/src/middlewares/checkAuth.js
@@ -1,8 +1,10 @@
 import jwt from "jsonwebtoken";
 import { SystemError } from "../utils/systemError.js";
+import { User } from "../../database/models/user.model.js";
 
 // check auth / verify token
 export const checkAuth = async (req, res, next) => {
+  let decodedToken;
   // 01- get token from headers
   const authHeader = req.headers.authorization || req.headers.Authorization;
   const token = authHeader?.split(" ")[1] || null;
@@ -12,19 +14,20 @@ export const checkAuth = async (req, res, next) => {
   }
   // 03- verify token
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return next(new SystemError("Invalid token", 401));
-    // 04- add the decoded user to the request object (modify the request object in middleware) - modifies/transforms the request object
-    req.user = decoded;
-    return next();
+    if (err) return next(new SystemError("Invalid token: " + err, 401));
+    decodedToken = decoded;
   });
   // 04- check if user exists by id
-  const user = await User.findById(req.user.id);
+  const user = await User.findById(decodedToken.id);
   if (!user) return next(new SystemError("User not found", 401));
   // 05- check if token is valid by comparing token iat (issued at) after last password change (updatedAt or passwordChangedAt) or last logout time (lastLoginAt)
   if (
-    user.passwordChangedAt > req.user.iat ||
-    user.lastLoginAt > req.user.iat
+    user?.passwordChangedAt.getTime() / 1000 > decodedToken.iat ||
+    user?.lastLoginAt.getTime() / 1000 > decodedToken.iat
   ) {
     return next(new SystemError("Token expired", 401));
   }
+  // 06- add the decoded user to the request object (modify the request object in middleware) - modifies/transforms the request object
+  req.user = decodedToken;
+  return next();
 };
