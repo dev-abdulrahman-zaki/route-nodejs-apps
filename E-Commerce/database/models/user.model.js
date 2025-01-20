f6e80f3d667c822f3bf4e072e822acf992a0bdd2
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-20 05:55:26 +0200

    Enhance user model with password hashing: Implement pre-save and pre-findOneAndUpdate middleware to hash passwords before saving or updating user documents. This improves security by ensuring passwords are stored securely. Additionally, comment out user routes in index.routes.js for future implementation.

--- E-Commerce/database/models/user.model.js
+++ E-Commerce/database/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"; // or: import {Schema, model} from "mongoose";
+import bcrypt from "bcrypt";
 
 // 01. Define the schema
 const userSchema = new mongoose.Schema(
@@ -43,7 +44,6 @@ const userSchema = new mongoose.Schema(
       default: "user",
       lowercase: true,
     },
-
   },
   {
     timestamps: {
@@ -53,5 +53,38 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-// 02. Define the model
+// 02-01. Hash the password before add
+/*
+ - "pre" it is a middleware that runs before the save operation.
+ - "save" because await user.save() in addUser controller.
+ - "this" is the current user instance (refers to the document being saved).
+ - `this.password` accesses the document's password field.
+ - "isModified" it is a mongoose method that checks if the password field is modified.
+ - "next" is a function that calls the next middleware in the chain.
+ - "return next()" because if the password is not modified, we don't need to hash it.
+*/
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// 02-02. Hash the password before update
+/*
+ - "pre" it is a middleware that runs before the save operation.
+ - "findOneAndUpdate" because await User.findByIdAndUpdate() in updateUser controller.
+ - "this" refers to the query object, not the document.
+ - "this._update" is the update object.
+ - The update data is in the query's update object
+ - We need to access the password through the update object
+ - ref: https://poe.com/s/6zkoh7VpRjMTox6ShGSH
+*/
+userSchema.pre("findOneAndUpdate", async function (next) {
+  if (this._update.password) {
+    this._update.password = await bcrypt.hash(this._update.password, 10);
+  }
+  next();
+});
+
+// 03. Define the model
 export const User = mongoose.model("User", userSchema); // User is the name of the collection in the "localhost:27017/mongoose" database, which is "users" by default.
--- E-Commerce/src/modules/index.routes.js
+++ E-Commerce/src/modules/index.routes.js
@@ -7,7 +7,7 @@ import { checkAuth } from "../middlewares/checkAuth.js";
 
 export const indexRoutes = (app) => {
   app.get("/", (req, res) => res.send("Hello World!"));
-  app.use("/api/v1/auth", userRoutes);
+  // app.use("/api/v1/auth", userRoutes);
   app.use("/api/v1/categories", checkAuth, categoryRoutes);
   app.use("/api/v1/subcategories", checkAuth, subCategoryRoutes);
   app.use("/api/v1/brands", checkAuth, brandRoutes);
