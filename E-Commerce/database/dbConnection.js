4f93eab1207be7644994fbb2d214bf4c3a47fdbe
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-22 01:48:53 +0200

    Move to MongoDB Atlas

--- E-Commerce/database/dbConnection.js
+++ E-Commerce/database/dbConnection.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose"; // or: import {connect} from "mongoose";
 
 // 01. Connect to MongoDB
 export const dbConnection = mongoose
-  .connect(process.env.MONGODB_URI_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/e-commerce') // or: mongodb://localhost:27017/e-commerce
+  .connect(process.env.MONGODB_URI_CONNECTION_STRING)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB Atlas");
   })
   .catch((err) => {
     console.error('Database connection error:', err);
