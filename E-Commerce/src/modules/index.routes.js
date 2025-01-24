7d8f068a9332c1389531d120be43886219d2d486
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2025-01-24 08:29:55 +0200

    remove checkAuth from index routes

--- E-Commerce/src/modules/index.routes.js
+++ E-Commerce/src/modules/index.routes.js
@@ -8,19 +8,17 @@ import reviewRoutes from "./review/review.routes.js";
 import wishlistRoutes from "./wishlist/wishlist.routes.js";
 import addressRoutes from "./address/address.routes.js";
 import couponRoutes from "./coupon/coupon.routes.js";
-// todo: maybe remove checkAuth from here
-import { checkAuth } from "../middlewares/checkAuth.js";
 
 export const indexRoutes = (app) => {
   app.get("/", (req, res) => res.send("Hello World!"));
   app.use("/api/v1/auth", authRoutes);
-  app.use("/api/v1/categories", checkAuth, categoryRoutes);
-  app.use("/api/v1/subcategories", checkAuth, subCategoryRoutes);
-  app.use("/api/v1/brands", checkAuth, brandRoutes);
-  app.use("/api/v1/products", checkAuth, productRoutes);
-  app.use("/api/v1/users", checkAuth, userRoutes);
-  app.use("/api/v1/reviews", checkAuth, reviewRoutes);
-  app.use("/api/v1/wishlists", checkAuth, wishlistRoutes);
-  app.use("/api/v1/addresses", checkAuth, addressRoutes);
-  app.use("/api/v1/coupons", checkAuth, couponRoutes);
+  app.use("/api/v1/categories", categoryRoutes);
+  app.use("/api/v1/subcategories", subCategoryRoutes);
+  app.use("/api/v1/brands", brandRoutes);
+  app.use("/api/v1/products", productRoutes);
+  app.use("/api/v1/users", userRoutes);
+  app.use("/api/v1/reviews", reviewRoutes);
+  app.use("/api/v1/wishlists", wishlistRoutes);
+  app.use("/api/v1/addresses", addressRoutes);
+  app.use("/api/v1/coupons", couponRoutes);
 };
